b8d566bc7c621042fdc642b352175270a0f60341
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2021-12-24 13:56:07 -0500

    feat: add/remove subscriptions

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 var (
 	method = flag.String("method", "playlists", "The APi Method to execute (default: playlists)")
 	mine   = flag.Bool("mine", true, "List playlist for authenticated user (default: true)")
+
+	channelId = flag.String("channel", "", "add-subscription, channelId of subscription")
+
+	subscriptionId = flag.String("subscription", "", "rm-subscription, subscriptionId of subscription")
 )
 
 const (
@@ -39,6 +43,65 @@ func playList(service *youtube.Service) {
 	}
 }
 
+func subscriptionList(service *youtube.Service) {
+	format := "%-43v\t%-24v\t%-20v\t%v\n"
+
+	call := service.Subscriptions.List([]string{"snippet"})
+	call.Mine(*mine)
+	call.MaxResults(maxResults)
+
+	fmt.Printf(format, "Id", "ResorceId", "Kind", "Title")
+	err := call.Pages(context.Background(), func(response *youtube.SubscriptionListResponse) error {
+		for _, subscription := range response.Items {
+			fmt.Printf(format, subscription.Id, subscription.Snippet.ResourceId.ChannelId, subscription.Snippet.ResourceId.Kind, subscription.Snippet.Title)
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Unable to call service: %v", err)
+	}
+}
+
+func subscriptionAdd(service *youtube.Service) {
+	if *channelId == "" {
+		log.Fatalf("channelId is required")
+	}
+
+	subscription := youtube.Subscription{
+		Snippet: &youtube.SubscriptionSnippet{
+			ResourceId: &youtube.ResourceId{
+				ChannelId: *channelId,
+				Kind:      "youtube#channel",
+			},
+		},
+	}
+
+	call := service.Subscriptions.Insert([]string{"snippet"}, &subscription)
+	response, err := call.Do()
+
+	if err != nil {
+		log.Fatalf("Unable to create subscription: %v", err)
+	}
+
+	fmt.Printf("Subscription created: %v", response.Id)
+}
+
+func subscriptionDel(service *youtube.Service) {
+	if *subscriptionId == "" {
+		log.Fatalf("resourceId is required")
+	}
+
+	call := service.Subscriptions.Delete(*subscriptionId)
+	err := call.Do()
+
+	if err != nil {
+		log.Fatalf("Unable to remove subscription: %v", err)
+	}
+
+	fmt.Printf("Subscription deleted")
+}
+
 func main() {
 	flag.Parse()
 
@@ -54,25 +117,9 @@ func main() {
 		playList(service)
 	case "subscriptions":
 		subscriptionList(service)
-	}
-}
-
-func subscriptionList(service *youtube.Service) {
-	format := "%-43v\t%-24v\t%v\n"
-
-	call := service.Subscriptions.List([]string{"snippet"})
-	call.Mine(*mine)
-	call.MaxResults(maxResults)
-
-	fmt.Printf(format, "Id", "ResorceId", "Title")
-	err := call.Pages(context.Background(), func(response *youtube.SubscriptionListResponse) error {
-		for _, playlist := range response.Items {
-			fmt.Printf(format, playlist.Id, playlist.Snippet.ResourceId.ChannelId, playlist.Snippet.Title)
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatalf("Unable to call service: %v", err)
+	case "add-subscription":
+		subscriptionAdd(service)
+	case "rm-subscription":
+		subscriptionDel(service)
 	}
 }
